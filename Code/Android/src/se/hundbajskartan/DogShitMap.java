ac56aab5d62baaa0b117144752ed9c3b63e1bf91
Magnus Bondesson <m4gnus@gmail.com>
2011-04-29 01:05:18 +0200

    Now using GPS and plotting a shit marker on the map

--- Code/Android/src/se/hundbajskartan/DogShitMap.java
+++ Code/Android/src/se/hundbajskartan/DogShitMap.java
@@ -1,65 +1,57 @@
 package se.hundbajskartan;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
-import android.app.Activity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
+import se.hundbajskartan.classlibrary.DogShit;
 import android.content.Intent;
-import android.location.Criteria;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class DogShitMap extends Activity
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+import com.google.gson.Gson;
+
+public class DogShitMap extends MapActivity
 {
-	// private static final int CAMERA_PIC_REQUEST=1337;
-	//
-	//
-	// /** Called when the activity is first created. */
-	// @Override
-	// public void onCreate(Bundle savedInstanceState)
-	// {
-	// super.onCreate(savedInstanceState);
-	// setContentView(R.layout.main);
-	//
-	// takePicture();
-	//
-	// Button takePicture=(Button)findViewById(R.id.btn_take_picture);
-	// takePicture.setOnClickListener(new View.OnClickListener()
-	// {
-	// @Override
-	// public void onClick(View v)
-	// {
-	// takePicture();
-	// }
-	// });
-	// }
-	//
-	//
-	// private void takePicture()
-	// {
-	// Intent cameraIntent=new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-	// startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
-	// }
-	//
-	//
-	// protected void onActivityResult(int requestCode, int resultCode, Intent data)
-	// {
-	// if (requestCode==CAMERA_PIC_REQUEST)
-	// {
-	// // do something
-	// Bitmap thumbnail=(Bitmap)data.getExtras().get("data");
-	// ImageView image=(ImageView)findViewById(R.id.iv_photo_result);
-	// image.setImageBitmap(thumbnail);
-	// }
-	private static final int CAMERA_PIC_REQUEST=1337;
+	private final String TAG = "DOGSHITMAP";
+	private final String URL = "http://hundbajsar.appspot.com/dogshitmapserver";
+	// private final String URL = "http://74.125.77.141/dogshitmapserver";
 
+	private MapView mMapView;
+	private TextView mTextViewResult;
+	private TextView mTextViewGPSStatus;
+	private LocationManager locationManager;
+	private LocationListener locationListener;
+	private MapController mapController;
+	private List<Overlay> mapOverlays;
+	private HelloItemizedOverlay itemizedoverlay;
+	private Button mButtonGetLocation;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -68,192 +60,291 @@ public class DogShitMap extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		Button takePicture=(Button)findViewById(R.id.btn_take_picture);
-		takePicture.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				try
-				{
-					takePicture();
-					Toast.makeText(getApplicationContext(), "Camera's OK button not working yet!", Toast.LENGTH_LONG).show();
-				}
-				catch (Exception ex)
-				{
-					Logger.LogError(ex.toString());
-				}
-			}
-		});
+		mButtonGetLocation = (Button) findViewById(R.id.btn_get_location);
+		mTextViewResult = (TextView)findViewById(R.id.tv_result_value);
+		mTextViewGPSStatus = (TextView)findViewById(R.id.tv_gps_status);
+		mMapView = (MapView) findViewById(R.id.map_view);
+		mapController = mMapView.getController();
+		mapController.setZoom(11);
+		setMapCenter(18.04, 59.35);
+		
+		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		locationListener = new LocationListener() {
+		    @Override
+			public void onLocationChanged(Location location) {
+		      // Called when a new location is found.
+		      makeUseOfNewLocation(location);
+		    }
 
-		Button getLocation=(Button)findViewById(R.id.btn_get_location);
-		getLocation.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				try
-				{
-					Location location=getLocation();
-					if (location!=null)
-						sendEmail(location);
-				}
-				catch (Exception ex)
-				{
-					Logger.LogError(ex.toString());
-				}
-			}
-		});
+		    @Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+		    	switch (status) {
+	            case LocationProvider.AVAILABLE:
+            		mTextViewGPSStatus.setText(mTextViewGPSStatus.getText().toString() + "GPS available again\n");
+	                break;
+	            case LocationProvider.OUT_OF_SERVICE:
+	            	mTextViewGPSStatus.setText(mTextViewGPSStatus.getText().toString() + "GPS out of service\n");
+	                break;
+	            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+	            	mTextViewGPSStatus.setText(mTextViewGPSStatus.getText().toString() + "GPS temporarily unavailable\n");
+	                break;
+	            }
+		    }
+
+		    @Override
+			public void onProviderEnabled(String provider) {
+		    	Log.i(TAG, "Provider enabled");
+		    }
+
+		    @Override
+			public void onProviderDisabled(String provider) {
+		    	Log.i(TAG, "Provider disabled");
+		    }
+		  };
+
+		mapOverlays = mMapView.getOverlays();
+		Drawable drawable = this.getResources().getDrawable(
+R.drawable.poop);
+		itemizedoverlay = new HelloItemizedOverlay(
+				drawable);
 	}
 
+	private void setMapCenter(Double longitude, Double latitude) {
+		mapController.setCenter(new GeoPoint((int) (latitude * 1000000),
+				(int) (longitude * 1000000)));
+	}
 
-	private void takePicture()
-	{
-		// Intent cameraIntent=new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-		// startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
-		Intent i=new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-		if (hasImageCaptureBug())
+	protected void makeUseOfNewLocation(Location location) {
+		if (location!=null)
 		{
-			i.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File("/sdcard/tmp")));
+			mTextViewResult.setText(location.getLongitude() + " " + location.getLatitude());
+
+			DogShit dogshit = new DogShit(location.getLongitude(),
+					location.getLatitude(), new Date());
+			sendJson(dogshit);
+
+			setMapCenter(dogshit.getLongitude(), dogshit.getLatitude());
+			mapController.setZoom(19);
+			GeoPoint point = new GeoPoint((int) (dogshit.getLatitude() * 1E6),
+					(int) (dogshit.getLongitude() * 1E6));
+			OverlayItem overlayitem = new OverlayItem(point, "Bajs!",
+					"Här vare bajs!");
+
+			itemizedoverlay.addOverlay(overlayitem);
+			mapOverlays.add(itemizedoverlay);
+			mMapView.invalidate();
+			// mMapView.
+			// sendJsonWithGson(location);
 		}
 		else
 		{
-			i.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			mTextViewResult.setText("No position available.");
 		}
-		startActivityForResult(i, CAMERA_PIC_REQUEST);
+
+		mButtonGetLocation.setEnabled(true);
+		locationManager.removeUpdates(locationListener);
 	}
 
-
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent)
+	public void getLocationClick(View view)
 	{
-		switch (requestCode)
+		try
 		{
-			// case GlobalConstants.IMAGE_CAPTURE:
-			case CAMERA_PIC_REQUEST:
-				Uri u;
-				if (hasImageCaptureBug())
-				{
-					File fi=new File("/sdcard/tmp");
-					try
-					{
-						u=Uri
-								.parse(android.provider.MediaStore.Images.Media.insertImage(getContentResolver(), fi.getAbsolutePath(), null,
-																							null));
-						if (!fi.delete())
-							Log.i(Globals.DSM_TAG, "Failed to delete "+fi);
-					}
-					catch (FileNotFoundException e)
-					{
-						e.printStackTrace();
-					}
-				}
-				else
-				{
-					u=intent.getData();
-				}
+			// MOCK GPS
+			// ProgressDialog dialog = ProgressDialog.show(DogShitMap.this, "",
+			// "Getting position. Please wait...", true);
+			// DogShit dogshit = createFakeDogShit();
+			// if (dogshit != null)
+			// {
+			// mTextViewResult.setText(dogshit.getLongitude() + " "
+			// + dogshit.getLatitude());
+			// sendJson(dogshit);
+				// Show position on map
+				// GeoPoint point = new GeoPoint(dogshit.getLatitude()*1000000,
+				// Integer.parseInt((dogshit.getLongitude()*1000000.toString()
+			// }
 
-				// Bitmap thumbnail=(Bitmap)intent.getExtras().get("data");
-				// ImageView image=(ImageView)findViewById(R.id.iv_photo_result);
-				// image.setImageBitmap(thumbnail);
+			// ORIGINAL CODE, will be used when real GPS data is required
+			// Register the listener with the Location Manager to receive location updates
+			// new GetPosition().execute();
+			mButtonGetLocation.setEnabled(false);
+			locationManager.requestLocationUpdates(
+					LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+		}
+		catch (Exception ex)
+		{
+			Logger.LogError(ex.toString());
 		}
 	}
 
+	// private class GetPosition extends AsyncTask<String, Void, Void> {
+	// private final HttpClient Client = new DefaultHttpClient();
+	// private String Content;
+	// private String Error = null;
+	// private final ProgressDialog Dialog = new ProgressDialog(
+	// DogShitMap.this);
+	//
+	// @Override
+	// protected void onPreExecute() {
+	// Dialog.setMessage("Getting position...");
+	// Dialog.show();
+	// }
+	//
+	// protected Void doInBackground() {
+	// try {
+	// // HttpGet httpget = new HttpGet(urls[0]);
+	// // ResponseHandler<String> responseHandler = new
+	// // BasicResponseHandler();
+	// // Content = Client.execute(httpget, responseHandler);
+	// } catch (ClientProtocolException e) {
+	// Error = e.getMessage();
+	// cancel(true);
+	// } catch (IOException e) {
+	// Error = e.getMessage();
+	// cancel(true);
+	// }
+	//
+	// return null;
+	// }
+	//
+	// @Override
+	// protected void onPostExecute(Void unused) {
+	// Dialog.dismiss();
+	// if (Error != null) {
+	// Toast.makeText(DogShitMap.this, Error, Toast.LENGTH_LONG)
+	// .show();
+	// } else {
+	// Toast.makeText(DogShitMap.this, "Source: " + Content,
+	// Toast.LENGTH_LONG).show();
+	// }
+	// }
+	// }
 
-	private void SavePicture()
-	{
-		// // Write the file to storage
-		// java.util.UUID uid=java.util.UUID.randomUUID();
-		// android.content.Context c=getContext();
-		//
-		// try
-		// {
-		// boolean mExternalStorageAvailable=false;
-		// boolean mExternalStorageWriteable=false;
-		// String state=Environment.getExternalStorageState();
-		//
-		// if (Environment.MEDIA_MOUNTED.equals(state))
-		// {
-		// // We can read and write the media
-		// mExternalStorageAvailable=mExternalStorageWriteable=true;
-		//
-		// Log.i("P_PC_WRITE.FILE", "Begin write file");
-		// File ff=new File(Environment.getExternalStorageDirectory(),
-		// "test"+uid.toString()+".jpg");
-		//
-		// OutputStream os=new FileOutputStream(ff);
-		//
-		// os.write(_data);
-		// os.close();
-		// }
-		// else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state))
-		// {
-		// // We can only read the media
-		// mExternalStorageAvailable=true;
-		// mExternalStorageWriteable=false;
-		// }
-		// else
-		// {
-		// // Something else is wrong. It may be one of many other states, but all we need
-		// // to know is we can neither read nor write
-		// mExternalStorageAvailable=mExternalStorageWriteable=false;
-		// }
-		// }
-		// catch (Exception ex)
-		// {
-		// // TODO: Error handling
-		// }
+	private DogShit createFakeDogShit() {
+		DogShit dogshit = null;
+		Random random = new Random();
+		final int latDecimals = random.nextInt(99);
+		final int lngDecimals = random.nextInt(99);
+
+		dogshit = new DogShit(18 + (lngDecimals / 100d),
+				59 + (latDecimals / 100d), new Date());
+
+		return dogshit;
 	}
-
-
-	private Location getLocation()
-	{
-		Location location;
-
-		LocationManager locManager=(LocationManager)getSystemService(LOCATION_SERVICE);
-		Criteria criteria=new Criteria();
-		criteria.setAccuracy(Criteria.ACCURACY_FINE);
-		String provider=locManager.getBestProvider(criteria, true);
-		location=locManager.getLastKnownLocation(provider);
-
-		return location;
-	}
-
 
 	private void sendEmail(Location location)
 	{
 		final Intent emailIntent=new Intent(android.content.Intent.ACTION_SEND);
 		emailIntent.setType("plain/text");
-		emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"m4gnus@gmail.com"});
+		emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"m4gnus@gmail.com", "robert.b.johansson@gmail.com"});
 		emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Hundbajskartan - bajs hittat och geotaggat");
 		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "http://maps.google.com/?q="+location.getLatitude()+","+location.getLongitude());
 		startActivity(Intent.createChooser(emailIntent, "Send mail..."));
 	}
-
-
-	// protected void onActivityResult(int requestCode, int resultCode, Intent data)
-	// {
-	// if (requestCode==CAMERA_PIC_REQUEST)
-	// {
-	// // do something
-	// Bitmap thumbnail=(Bitmap)data.getExtras().get("data");
-	// ImageView image=(ImageView)findViewById(R.id.iv_photo_result);
-	// image.setImageBitmap(thumbnail);
-	// }
-	// }
-
-	// }
-
-	public boolean hasImageCaptureBug()
+	
+	protected void sendJson(final DogShit dogshit) 
 	{
-		// list of known devices that have the bug
-		ArrayList<String> devices=new ArrayList<String>();
-		devices.add("android-devphone1/dream_devphone/dream");
-		devices.add("generic/sdk/generic");
-		devices.add("vodafone/vfpioneer/sapphire");
-		devices.add("tmobile/kila/dream");
-		devices.add("verizon/voles/sholes");
-		devices.add("google_ion/google_ion/sapphire");
+        Thread t = new Thread(){
+        @Override
+		public void run() {
+                Looper.prepare(); //For Preparing Message Pool for the child Thread
+                HttpClient client = new DefaultHttpClient();
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+                HttpResponse response;
+                JSONObject json = new JSONObject();
+                try
+                {
+                    HttpPost post = new HttpPost(URL);
+                    json.put("longitude", dogshit.getLongitude());
+                    json.put("latitude", dogshit.getLatitude());
+					json.put("date", dogshit.getDate());
+					Log.e(TAG, dogshit.getDate().toString());
+					StringEntity se = new StringEntity(json.toString());
+					// se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,
+					// "application/json"));
+                    post.setEntity(se);
+                    response = client.execute(post);
+                    /*Checking response */
+                    if(response!=null)
+ {
+						Log.d(TAG, response.getStatusLine().getStatusCode()
+								+ "  "
+								+ response.getStatusLine().getReasonPhrase()
+								+ "");
+						// Get the data in the entity
+						InputStream in = response.getEntity().getContent();
+					}
+                }
+                catch(Exception e){
+					Log.d(TAG, e.getStackTrace().toString());
+					// Toast.makeText(getContext(),
+					// e.getStackTrace().toString(), 20000);
+					// createDialog("Error", "Cannot Establish Connection");
+                }
+                Looper.loop(); //Loop in the message queue
+            }
+        };
+        t.start();      
+    }
 
-		return devices.contains(android.os.Build.BRAND+"/"+android.os.Build.PRODUCT+"/"+android.os.Build.DEVICE);
+	protected void sendJson(final String position) {
+		Thread t = new Thread() {
+			@Override
+			public void run() {
+				Looper.prepare(); // For Preparing Message Pool for the child
+									// Thread
+				HttpClient client = new DefaultHttpClient();
+				HttpConnectionParams.setConnectionTimeout(client.getParams(),
+						10000); // Timeout Limit
+				HttpResponse response;
+
+				try {
+					HttpPost post = new HttpPost(URL);
+					StringEntity se = new StringEntity("JSON: "
+ + position);
+					se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,
+							"application/json"));
+					post.setEntity(se);
+					response = client.execute(post);
+					/* Checking response */
+					if (response != null) {
+						// Get the data in the entity
+						InputStream in = response.getEntity().getContent();
+					}
+				} catch (Exception e) {
+					Log.d(TAG, e.getStackTrace().toString());
+					// createDialog("Error", "Cannot Establish Connection");
+				}
+				Looper.loop(); // Loop in the message queue
+			}
+		};
+		t.start();
+	}
+
+	protected void sendJsonWithGson(DogShit dogshit) {
+		// DogShit dogshit = new DogShit(location.getLongitude(),
+		// location.getLongitude(), new Date());
+		Gson gson = new Gson();
+
+		// Convert java object to JSON format
+		String json = gson.toJson(dogshit);
+
+		sendJson(json);
+	}
+
+	protected void sendJsonWithGson(Location location) {
+		DogShit dogshit = new DogShit(location.getLongitude(),
+				location.getLongitude(), new Date());
+		Gson gson = new Gson();
+
+		// Convert java object to JSON format
+		String json = gson.toJson(dogshit);
+
+		sendJson(json);
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
---
+++ Code/Android/src/se/hundbajskartan/HelloItemizedOverlay.java
@@ -0,0 +1,50 @@
+package se.hundbajskartan;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class HelloItemizedOverlay extends ItemizedOverlay {
+
+	private final ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context mContext;
+
+	public HelloItemizedOverlay(Drawable defaultMarker) {
+		super(boundCenterBottom(defaultMarker));
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+		mOverlays.add(overlay);
+		populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mOverlays.size();
+	}
+
+	public HelloItemizedOverlay(Drawable defaultMarker, Context context) {
+		super(defaultMarker);
+		mContext = context;
+	}
+
+	@Override
+	protected boolean onTap(int index) {
+		OverlayItem item = mOverlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
+}
