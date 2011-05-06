69162e1cf3543990832e31025c202da42b9eb5ba
Johannes Kaehlare <johannes@spotify.com>
2011-05-06 16:34:02 +0200

    Initial commit for the iphone release :), jippie\!

---
+++ Code/Iphone/Hundbajskartan/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Hundbajskartan
+//
+//  Created by Johannes Kaehlare on 5/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
